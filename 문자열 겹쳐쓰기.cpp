afeb39db5070d83f7d0249d868323566a07c5b3a
sky <89959016+yeohl@users.noreply.github.com>
2024-08-25 23:24:21 +0900

    Update 문자열 겹쳐쓰기.cpp

--- 문자열 겹쳐쓰기.cpp
+++ 문자열 겹쳐쓰기.cpp
@@ -1,9 +1,13 @@
 #include <string>
 #include <vector>
-
+#include <iostream>
 using namespace std;
 
 string solution(string my_string, string overwrite_string, int s) {
     string answer = "";
+    for(int i = s; i< s + overwrite_string.length(); i++){
+        my_string[i] = overwrite_string[i - s];
+        answer = my_string;
+    }
     return answer;
 }

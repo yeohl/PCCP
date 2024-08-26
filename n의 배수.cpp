5199c432d9a9150cfa479e39e5dd0e638495d4ff
sky <89959016+yeohl@users.noreply.github.com>
2024-08-26 22:20:07 +0900

    Update n의 배수.cpp

--- n의 배수.cpp
+++ n의 배수.cpp
@@ -1,9 +1,15 @@
 #include <string>
 #include <vector>
+#include <iostream>
 
 using namespace std;
 
 int solution(int num, int n) {
     int answer = 0;
+    if(num % n == 0){
+        answer = 1;
+    }else{
+        answer = 0;
+    }
     return answer;
 }

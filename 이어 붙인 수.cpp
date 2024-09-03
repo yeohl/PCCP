68566ec799d2ffb5d427854db880551956484e5b
sky <89959016+yeohl@users.noreply.github.com>
2024-09-04 00:26:02 +0900

    Update 이어 붙인 수.cpp

--- 이어 붙인 수.cpp
+++ 이어 붙인 수.cpp
@@ -1,11 +1,19 @@
 #include <string>
 #include <vector>
 #include <iostream>
+
 using namespace std;
 
 int solution(vector<int> num_list) {
     int answer = 0;
-
-    for(int i=0; i<
+    string a = 0, b = 0;
+    for(int i = 0; i<num_list.size(); i++){
+        if(num_list[i] % 2 == 0){
+            a += num_list[i];
+        }else{
+            b += num_list[i];
+        }
+        answer = a+b;
+    }
     return answer;
 }
